5edf962f9392163cdc9a7c99425689f06c05929f
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2020-06-23 09:17:39 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+import React from 'react';
+import ReactDom from 'react-dom';
+const YOURNAME = 'NOMUOJA OGHENEMARO';
+const CURRENTYEAR = new Date().getFullYear();
+ReactDom.render(<div> 
+  <p> created by {YOURNAME}</p>
+  <p>copyright {CURRENTYEAR} </p>
+  </div>,document.getElementById("root"))
